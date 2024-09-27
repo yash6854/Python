48f68679f248b538b86babf62034c9f1dfa18b2d
Yash Vijay Maral <maral.yash.2k@gmail.com>
2024-09-27 20:44:21 +0530

    Removed Space

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 
 from account_manager import AccountManager
 
-
 def main():
     manager = AccountManager()
     account1 = manager.create_account("savings", 101, "Mayur", 1000)
     account2 = manager.create_account("checking", 102, "Yash", 5000)
-
 
     print(f"Mayur's balance: {account1.get_balance()}")
     print(f"Yash's  balance: {account2.get_balance()}")
@@ -17,16 +15,8 @@ def main():
     print(f"Mayur's balance: {account1.get_balance()}")
     print(f"Yash's  balance: {account2.get_balance()}")
 
-
     print(account1.apply_interest())
     print(f"Mayur's balance: {account1.get_balance()}")
     
-    
-    
- 
-    
-    
-
-
 if __name__ == "__main__":
     main()
